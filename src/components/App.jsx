25947e6a19acc05a56724762861b9a25b2c41e6c
said-ops <idrissisaid29@gmail.com>
2024-09-05 19:16:33 +0100

    base layout

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,41 @@ import React from 'react'
 function App() {
   return (
     <>
-    <h1>App</h1>
+    <section className='form-container'>
+      <div className='left'>
+        <div className="step">
+          <div className="step-number">1</div>
+          <div className="step-name">
+            <span className='step-no'>Step 1</span>
+            <span className='step-title'>YOUR INFO</span>
+          </div>
+        </div>
+        <div className="step">
+          <div className="step-number">2</div>
+          <div className="step-name">
+            <span className='step-no'>Step 2</span>
+            <span className='step-title'>SELECT PLAN</span>
+          </div>
+        </div>
+        <div className="step">
+          <div className="step-number">3</div>
+          <div className="step-name">
+            <span className='step-no'>Step 3</span>
+            <span className='step-title'>ADD-ONES</span>
+          </div>
+        </div>
+        <div className="step">
+          <div className="step-number">4</div>
+          <div className="step-name">
+            <span className='step-no'>Step 4</span>
+            <span className='step-title'>SUMMARY</span>
+          </div>
+        </div>
+      </div>
+      <div className="right">
+        {/* compoents goes here */}
+      </div>
+    </section>
   
     </>
   )
