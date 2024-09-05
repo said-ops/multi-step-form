20dbe6d235cc30d6440f18c066bb2afe83e199ca
said-ops <idrissisaid29@gmail.com>
2024-09-05 18:13:40 +0100

    repo init

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import App from './components/App.jsx'
 import './style/reset.css'
 import './style/index.css'
 
